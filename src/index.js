2d15305c7b7ff5ac206f0a176ec46e291d88f06b
DESKTOP-AS3C2MR\Admin <vantuan19961997@gmail.com>
2023-01-17 18:20:51 +0700

    no message

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {I18nextProvider} from 'react-i18next';
 import i18n from './i18n';
 import common_vn from "./translations/vn/common.json";
 import common_en from "./translations/en/common.json";
-
+// aaa
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <I18nextProvider i18n={i18n}>
